421cfec0d21e49b28da1dbda97dafae02f09af13
Gaétan M-F <gaetan.mf@outlook.fr>
2020-05-05 15:03:54 +0200

    First bokeh graph

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from bokeh.plotting import figure, output_file, show
+from bokeh.embed import components
 
-# Create your views here.
 def homepage(request):
- return render(request, 'pages/base.html', {})
+
+    x = [1,2,3,4,5]
+    y = [1,2,3,4,5]
+    
+    plot = figure(title = "Test graph", x_axis_label = "X-axis", y_axis_label = "Y-axis", plot_width = 400, plot_height = 400)
+    plot.line(x,y,line_width = 2)
+    script, div = components(plot)
+    
+    
+    return render_to_response('pages/base.html', {"script":script,"div":div})
